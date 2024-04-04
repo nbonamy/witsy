a2eeec7edfddf4b112e20b0131d9daa95451aaad
Nicolas Bonamy <nicolas@Bonamy.fr>
2024-04-04 09:15:17 -0500

    dummy icon

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: 'assets/icon',
   },
   rebuildConfig: {},
   makers: [
