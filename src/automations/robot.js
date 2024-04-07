065209824c71cd2e80115703cf4e8fd1610ad7ca
Nicolas Bonamy <nicolas@bonamy.fr>
2024-04-06 23:29:00 -0500

    no robotjs for now

--- src/automations/robot.js
+++ src/automations/robot.js
@@ -1,6 +1,6 @@
 
-import { clipboard } from 'electron';
-import { keyTap } from 'robotjs';
+//import { clipboard } from 'electron';
+//import { keyTap } from 'robotjs';
 
 export default class RobotAutomator {
   
@@ -8,27 +8,27 @@ export default class RobotAutomator {
   }
 
   async moveCaretBelow() {
-    keyTap("right");
-    keyTap("enter");
-    keyTap("enter");
+    // keyTap("right");
+    // keyTap("enter");
+    // keyTap("enter");
   }
 
   async getSelectedText() {
-    const currentClipboardContent = clipboard.readText();
-    clipboard.clear();
-    keyTap("c", process.platform === "darwin" ? "command" : "control");
-    await new Promise((resolve) => setTimeout(resolve, 200));
-    const selectedText = clipboard.readText();
-    clipboard.writeText(currentClipboardContent);
-    return selectedText;
+    // const currentClipboardContent = clipboard.readText();
+    // clipboard.clear();
+    // keyTap("c", process.platform === "darwin" ? "command" : "control");
+    // await new Promise((resolve) => setTimeout(resolve, 200));
+    // const selectedText = clipboard.readText();
+    // clipboard.writeText(currentClipboardContent);
+    // return selectedText;
   }
 
   async pasteText(text) {
-    const currentClipboardContent = clipboard.readText();
-    clipboard.writeText(text)
-    keyTap("v", process.platform === "darwin" ? "command" : "control");
-    await new Promise((resolve) => setTimeout(resolve, 200));
-    clipboard.writeText(currentClipboardContent);
+    // const currentClipboardContent = clipboard.readText();
+    // clipboard.writeText(text)
+    // keyTap("v", process.platform === "darwin" ? "command" : "control");
+    // await new Promise((resolve) => setTimeout(resolve, 200));
+    // clipboard.writeText(currentClipboardContent);
   }
 
 }
