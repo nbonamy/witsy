fd7e9184b2de524abd9fb30a8d9a499d94531147
Nicolas Bonamy <nicolas@bonamy.fr>
2024-04-09 21:58:26 -0500

    windows css fix

--- src/screens/Wait.vue
+++ src/screens/Wait.vue
@@ -12,16 +12,23 @@ import Loader from '../components/Loader.vue'
 
 <style scoped>
 
+::-webkit-scrollbar {
+  display: none;
+}
+
 .wait {
+  box-sizing: border-box;
   height: 100vh;
   background-color: #e7e6e5;
-  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   padding: 2px 8px;
   font-size: 9pt;
 }
 
 .loader {
-  margin: 1px;
+  margin: 4px;
   width: 6px;
   height: 6px;
   display: inline-block;
