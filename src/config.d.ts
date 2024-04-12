0024b97b10b3bf2cdc8583f0bc5237fe41ff8d59
Nicolas Bonamy <nicolas@bonamy.fr>
2024-04-12 09:48:13 -0500

    more typescript

---
+++ src/config.d.ts
@@ -0,0 +1,71 @@
+
+import { Shortcut } from './index.d'
+
+interface Configuration {
+  general: GeneralConfig
+  llm: LLMConfig
+  instructions: InstructionsConfig
+  appearance: AppearanceConfig
+  shortcuts: ShortcutsConfig
+  engines: {[key:string]: EngineConfig}
+  getActiveModel: () => string
+}
+
+interface GeneralConfig {
+  keepRunning: boolean
+  language: string
+}
+
+interface LLMConfig {
+  engine: string
+  autoVisionSwitch: boolean
+  conversationLength: number
+}
+
+interface InstructionsConfig {
+  default: string
+  routing: string
+  titling: string
+}
+
+interface AppearanceConfig {
+  chat: ChatAppearance
+}
+
+interface ChatAppearance {
+  theme: string
+  fontSize: number
+}
+
+interface ShortcutsConfig {
+  chat: Shortcut
+  command: Shortcut
+}
+
+interface EngineConfig {
+  apiKey?: string
+  models: ModelsConfig
+  model: ModelConfig
+  tts?: TTSConfig
+}
+
+interface Model {
+  id: string
+  name: string
+  meta: any
+}
+
+interface ModelsConfig {
+  chat: Model[]
+  image?: Model[]
+}
+
+interface ModelConfig {
+  chat: string
+  image: string
+}
+
+interface TTSConfig {
+  model: string
+  voice: string
+}
