2bb144728ec266460601ecbfba4ebfbf5a864caa
Nicolas Bonamy <nicolas@bonamy.fr>
2024-04-15 14:26:56 -0500

    more tests

---
+++ tests/engine_openai.test.ts
@@ -0,0 +1,52 @@
+
+import { beforeEach, expect, test } from 'vitest'
+import { store } from '../src/services/store'
+import defaults from '../defaults/settings.json'
+import Message from '../src/models/message'
+import OpenAI from '../src/services/openai'
+import { ChatCompletionChunk } from 'openai/resources'
+
+beforeEach(() => {
+  store.config = defaults
+  store.config.engines.openai.apiKey = '123'
+})
+
+test('OpenAI Basic', () => {
+  const openAI = new OpenAI(store.config)
+  expect(openAI.getName()).toBe('openai')
+  expect(openAI.isVisionModel('gpt-3.5')).toBe(false)
+  expect(openAI.isVisionModel('gpt-3.5-turbo')).toBe(false)
+  expect(openAI.isVisionModel('gpt-4-turbo')).toBe(true)
+  expect(openAI.isVisionModel('gpt-4-vision')).toBe(true)
+  expect(openAI.isVisionModel('gpt-4-vision-preview')).toBe(true)
+  expect(openAI.getRountingModel()).toMatch(/3.5/)
+})
+
+test('OpenAI addImageToPayload', async () => {
+  const openAI = new OpenAI(store.config)
+  const message = new Message('user', 'text')
+  message.attachFile({ type: 'image', url: '', format:'png', contents: 'image', downloaded: true })
+  const payload = { role: 'user', content: message }
+  openAI.addImageToPayload(message, payload)
+  expect(payload.content).toStrictEqual([
+    { type: 'text', text: 'text' },
+    { type: 'image_url', image_url: { url: 'data:image/jpeg;base64,image' } }
+  ])
+})
+
+test('OpenAI streamChunkToLlmChunk Text', async () => {
+  const openAI = new OpenAI(store.config)
+  const streamChunk: ChatCompletionChunk = {
+    id: 'id',
+    created: 1,
+    model: 'model',
+    object: 'chat.completion.chunk',
+    choices: [{ index: 0, delta: { content: 'response' }, finish_reason: null }],
+  }
+  const llmChunk1 = await openAI.streamChunkToLlmChunk(streamChunk, null)
+  expect(llmChunk1).toStrictEqual({ text: 'response', done: false })
+  streamChunk.choices[0].delta.content = null
+  streamChunk.choices[0].finish_reason = 'stop'
+  const llmChunk2 = await openAI.streamChunkToLlmChunk(streamChunk, null)
+  expect(llmChunk2).toStrictEqual({ text: '', done: true })
+})
--- tests/engine_plugins.test.ts
+++ tests/engine_plugins.test.ts
@@ -7,6 +7,13 @@ import LlmMock from './mocks/llm'
 beforeEach(() => {
   store.config = defaults
   store.config.llm.engine = 'mock'
+  store.config.plugins.browse = {
+    enabled: true,
+  }
+  store.config.plugins.tavily = {
+    enabled: true,
+    apiKey: '123',
+  }
   store.config.plugins.python = {
     enabled: true,
     binpath: 'python3',
@@ -15,22 +22,60 @@ beforeEach(() => {
 
 test('Plugins', () => {
   const llm = new LlmMock(store.config)
-  expect(llm.getAvailableTools()).toStrictEqual([{
-    type: 'function',
-    function: {
-      name: 'run_python_code',
-      description: 'Execute Python code and return the result',
-      parameters: {
-        type: 'object',
-        properties: {
-          'script': {
-            type: 'string',
-            'enum': undefined,
-            description: 'The script to run',
-          }
+  expect(llm.getAvailableTools()).toStrictEqual([
+    {
+      type: 'function',
+      function: {
+        name: 'get_html_as_text',
+        description: 'Download an HTML page and return the text content',
+        parameters: {
+          type: 'object',
+          properties: {
+            'url': {
+              type: 'string',
+              'enum': undefined,
+              description: 'The URL of the page to download',
+            }
+          },
+          required: ['url'],
         },
-        required: ['script'],
       },
     },
-  }])
+    {
+      type: 'function',
+      function: {
+        name: 'run_python_code',
+        description: 'Execute Python code and return the result',
+        parameters: {
+          type: 'object',
+          properties: {
+            'script': {
+              type: 'string',
+              'enum': undefined,
+              description: 'The script to run',
+            }
+          },
+          required: ['script'],
+        },
+      },
+    },
+    {
+      type: 'function',
+      function: {
+        name: 'search_tavily',
+        description: 'This tool allows you to search the web for information on a given topic',
+        parameters: {
+          type: 'object',
+          properties: {
+            'query': {
+              type: 'string',
+              'enum': undefined,
+              description: 'The query to search for',
+            }
+          },
+          required: ['query'],
+        },
+      },
+    },
+  ])
 })
