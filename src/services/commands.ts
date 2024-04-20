6883d877527c76f94c15a49f0da9f70df933d206
Nicolas Bonamy <nicolas@bonamy.fr>
2024-04-20 17:19:39 -0500

    load commands rework

--- src/services/commands.ts
+++ src/services/commands.ts
@@ -3,36 +3,6 @@ import { Command } from '../types/index.d'
 import { store } from './store'
 import defaultCommands from '../../defaults/commands.json'
 
-export const installCommands = () => {
-
-  // read file
-  let commands: Command[] = []
-  try {
-    commands = window.api.commands.load()
-  } catch (error) {
-    if (error.code !== 'ENOENT') {
-      console.log('Error retrieving commands data', error)
-    }
-  }
-
-  // now add new commands
-  for (const command of defaultCommands) {
-    const c = commands.find((cmd: Command) => cmd.id === command.id)
-    if (c == null) {
-      commands.push(command as Command)
-    } else {
-      if (c.shortcut === undefined) {
-        c.shortcut = command.shortcut || ''
-      }
-    }
-  }
-
-  // save
-  store.commands = commands
-  saveCommands()
-
-}
-
 export const newCommand = (): Command => {
   return {
     id: null,
@@ -48,13 +18,19 @@ export const newCommand = (): Command => {
   }
 }
 
+export const loadCommands = () => {
+  try {
+    return window.api.commands.load()
+  } catch (error) {
+    console.log('Error loading commands data', error)
+    return JSON.parse(JSON.stringify(defaultCommands))
+  }
+}
+
 export const saveCommands = () => {
-  
-  // save file
   try {
     window.api.commands.save(JSON.parse(JSON.stringify(store.commands)))
   } catch (error) {
     console.log('Error saving commands data', error)
   }
-
 }
