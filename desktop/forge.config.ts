591fbfeae0d4ccadeb79f1e574008d461cc5dc04
Nicolas Bonamy <nicolas@bonamy.fr>
2024-04-21 09:25:37 -0500

    packaging stuff for mas

--- desktop/forge.config.ts
+++ desktop/forge.config.ts
@@ -1,6 +1,7 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
+import { MakerPKG } from '@electron-forge/maker-pkg';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { VitePlugin } from '@electron-forge/plugin-vite';
@@ -19,6 +20,8 @@ const config: ForgeConfig = {
     executableName: 'Maiestro',
     appBundleId: 'com.nabocorp.maiestro',
     extendInfo: './assets/Info.plist',
+    osxUniversal: {
+    },
     osxSign: {
       identity: process.env.IDENTITY,
       optionsForFile: () => { return {
@@ -33,7 +36,9 @@ const config: ForgeConfig = {
     }
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [new MakerSquirrel({}), new MakerPKG({
+    identity: process.env.IDENTITY2,
+  }, ['darwin']), new MakerRpm({}), new MakerDeb({})],
   plugins: [
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
