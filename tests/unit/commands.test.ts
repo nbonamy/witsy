d9c2c604902c9174ea78e90a41202f0ebfd9919d
Nicolas Bonamy <nicolas@bonamy.fr>
2024-09-02 14:08:55 -0500

    more tests

--- tests/unit/commands.test.ts
+++ tests/unit/commands.test.ts
@@ -1,14 +1,34 @@
 
 import { vi, beforeAll, expect, test } from 'vitest'
+import { Command } from 'types'
 import { store } from '../../src/services/store'
-import * as commands from '../../src/services/commands'
+import * as main from '../../src/main/commands'
+import * as service from '../../src/services/commands'
+import defaultCommands from '../../defaults/commands.json'
+import { app } from 'electron'
+
+vi.mock('electron', async() => {
+  return {
+    app: {
+      getPath: vi.fn(() => '')
+    },
+  }
+})
+
+vi.mock('fs', async (importOriginal) => {
+  const mod: any = await importOriginal()
+  return { default: {
+    ...mod,
+    writeFileSync: vi.fn(),
+  }}
+})
 
 beforeAll(() => {
 
   // api
   window.api = {
     commands: {
-      load: vi.fn(() => []),
+      load: vi.fn(() => defaultCommands as Command[]),
       save: vi.fn(),
     }
   }
@@ -16,7 +36,7 @@ beforeAll(() => {
 })
 
 test('New command', () => {
-  const command = commands.newCommand()
+  const command = service.newCommand()
   expect(command).toStrictEqual({
     id: null,
     type: 'user',
@@ -31,13 +51,32 @@ test('New command', () => {
   })
 })
 
-test('Install commands', () => {
-  commands.loadCommands()
-  expect(window.api.commands.load).toHaveBeenCalled()
-  expect(store.commands).toStrictEqual([])
+test('Load default commands', () => {
+  const commands = main.loadCommands(app)
+  expect(commands).toHaveLength(39)
+  commands.forEach((command) => {
+    expect(command).toHaveProperty('id')
+    expect(command.type).toEqual('system')
+    expect(command.state).toEqual('enabled')
+  })
 })
 
-test('Save commands', () => {
-  commands.saveCommands()
+test('Load custom commands', () => {
+  const commands = main.loadCommands('./tests/fixtures/commands1.json')
+  expect(commands).toHaveLength(40)
+  expect(commands.filter(c => c.type === 'user')).toHaveLength(1)
+  expect(commands.filter(c => c.type === 'system')).toHaveLength(39)
+  expect(commands.filter(c => c.state === 'deleted')).toHaveLength(1)
+  expect(commands.filter(c => c.state === 'disabled')).toHaveLength(1)
+})
+
+test('Service Install commands', () => {
+  service.loadCommands()
+  expect(window.api.commands.load).toHaveBeenCalled()
+  expect(store.commands).toHaveLength(39)
+})
+
+test('Service Save commands', () => {
+  service.saveCommands()
   expect(window.api.commands.save).toHaveBeenCalled()
 })
