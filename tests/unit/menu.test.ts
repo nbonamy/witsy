860dab03116e9103bfe43a4fc9d6526d9d74f5f2
Nicolas Bonamy <nicolas@Bonamy.fr>
2024-09-09 13:01:13 -0500

    menu test

---
+++ tests/unit/menu.test.ts
@@ -0,0 +1,24 @@
+
+import { test, expect, vi } from 'vitest'
+import { app, Menu } from 'electron'
+import { installMenu } from '../../src/main/menu'
+
+vi.mock('electron', () => ({
+  app: { },
+  Menu: {
+    buildFromTemplate: vi.fn(() => { return {}}),
+    setApplicationMenu: vi.fn()
+  }
+}))
+
+test('installMenu', () => {
+  installMenu(app, {
+    quit: vi.fn(),
+    checkForUpdates: vi.fn(),
+    newChat: vi.fn(),
+    settings: vi.fn(),
+
+  })
+  expect(Menu.buildFromTemplate).toHaveBeenCalled()
+  expect(Menu.setApplicationMenu).toHaveBeenCalledWith({})
+})
