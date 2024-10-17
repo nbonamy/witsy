5153c9109c0c062b818faebc3dce692d652ebf78
Nicolas Bonamy <nicolas@bonamy.fr>
2024-10-17 17:39:26 -0500

    web worker test

---
+++ src/services/llm-worker.ts
@@ -0,0 +1,63 @@
+
+import { Configuration } from 'types/config'
+import LlmEngine from './engine'
+import { igniteEngine } from './llm'
+import { LlmChunk, LlmEvent } from 'types/llm'
+
+const worker: Worker = self as unknown as Worker
+
+let llm: LlmEngine = null
+
+const initEngine = (engine: string, config: Configuration) => {
+  llm = igniteEngine(engine, config)
+  llm.loadPlugins()
+}
+
+const stream = async (messages: any[], opts: any) => {
+
+  try {
+    let stream = await llm.stream(messages, opts)
+    while (stream) {
+      let newStream = null
+      for await (const streamChunk of stream) {
+        const chunk: LlmChunk = await llm.streamChunkToLlmChunk(streamChunk, (event: LlmEvent) => {
+          if (event.type === 'stream') {
+            newStream = event.content
+          } else  if (event.type === 'tool') {
+            worker.postMessage({ type: 'tool', content: event.content })
+          }
+        })
+        worker.postMessage({ type: 'chunk', chunk: chunk })
+      }
+      stream = newStream
+    }
+  } catch (error) {
+    console.error('Error while generating text', error)
+    worker.postMessage({ type: 'error', error: error })
+  }    
+
+}
+
+worker.addEventListener('message', (event: MessageEvent<any>) => {
+
+  // log
+  console.log('Worker received message', JSON.stringify(event.data).substr(0, 100))
+
+  // init
+  if (event.data.type === 'init') {
+    initEngine(event.data.engine, event.data.config)
+    return
+  }
+
+  // stream
+  if (event.data.type === 'stream') {
+    stream(event.data.messages, event.data.opts)
+    return
+  }
+
+  // too bad
+  console.log('Unknown message type', event.data.type)
+
+})
+
+export default worker
