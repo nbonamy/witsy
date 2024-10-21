83f558a96c19c4dd7ffa86914846bf1e8835d931
Nicolas Bonamy <nicolas@bonamy.fr>
2024-10-21 17:26:39 -0500

    more tests

---
+++ tests/unit/tips.test.ts
@@ -0,0 +1,83 @@
+
+import { vi, test, expect, beforeEach } from 'vitest'
+import { store } from '../../src/services/store'
+import Dialog from '../../src/composables/dialog'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+
+vi.mock('sweetalert2/dist/sweetalert2.js', async () => {
+  const Swal = vi.fn()
+  Swal.fire = vi.fn(() => Promise.resolve({ isConfirmed: true, isDenied: false, isDismissed: false }))
+  return { default: Swal }
+})
+
+beforeEach(() => {
+
+  window.api = {
+    showDialog: vi.fn(() => Promise.resolve({ response: 1, checkboxChecked: false })),
+    config: {
+      save: vi.fn(),
+    },
+  }
+
+  store.config = {
+    general: {
+      firstRun: true,
+      tips: {
+        scratchpad: true,
+        conversation: false,
+      }
+    }
+  }
+})
+
+test('Basic confirm', () => {
+  Dialog.alert('Hello')
+  expect(window.api.showDialog).toHaveBeenCalledWith({
+    type: 'none',
+    message: 'Hello',
+    detail: undefined,
+    buttons: [ 'OK' ],
+    defaultId: 0,
+    cancelId: -1,
+  })
+})
+
+test('Confirm/Cancel', () => {
+  Dialog.show({ title: 'Hello', text: 'World', showCancelButton: true, confirmButtonText: 'Yes' })
+  expect(window.api.showDialog).toHaveBeenCalledWith({
+    type: 'none',
+    message: 'Hello',
+    detail: 'World',
+    buttons: [ 'Yes', 'Cancel' ],
+    defaultId: 0,
+    cancelId: 1,
+  })
+})
+
+test('Confirm/Deny/Cancel', () => {
+  Dialog.show({ title: 'Hello', text: 'World', showCancelButton: true, showDenyButton: true, confirmButtonText: 'Yes', denyButtonText: 'No', cancelButtonText: 'Cancel' })
+  expect(window.api.showDialog).toHaveBeenCalledWith({
+    type: 'none',
+    message: 'Hello',
+    detail: 'World',
+    buttons: [ 'Yes', 'No', 'Cancel' ],
+    defaultId: 0,
+    cancelId: 2,
+  })
+})
+
+test('Input', () => {
+  Dialog.show({ title: 'Hello', text: 'World', input: 'text' })
+  expect(Swal.fire).toHaveBeenCalledWith({
+    customClass: {
+      cancelButton: 'alert-neutral',
+      denyButton: 'alert-danger',
+      confirmButton: 'alert-neutral',
+    },
+    iconHtml: '<img src="">',
+    input: 'text',
+    text: 'World',
+    title: 'Hello',
+    willOpen: expect.any(Function),
+  })
+})
