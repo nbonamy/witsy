ff2a783bf5db36f93202a092449678bd979dc630
Nicolas Bonamy <nicolas@bonamy.fr>
2024-10-20 20:39:05 -0500

    build darwin test

--- build/import_apple_cert.sh
+++ build/import_apple_cert.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env sh
 
-KEY_CHAIN=build.keychain
-CERTIFICATE_CER=certificate.cer
+KEYCHAIN_PATH=$RUNNER_TEMP/build.keychain-db
+CERTIFICATE_PATH=$RUNNER_TEMP/certificate.p12
 
 # list signing certificates
 security find-identity -v -p codesigning
 
 # recreate the certificate from the secure environment variable
-echo $IDENTITY_DARWIN_CER | base64 --decode > $CERTIFICATE_CER
+echo $BUILD_CERTIFICATE_BASE64 | base64 --decode -o $CERTIFICATE_PATH
 
 # create a keychain
-security create-keychain -p actions $KEY_CHAIN
-security default-keychain -s $KEY_CHAIN
-security unlock-keychain -p actions $KEY_CHAIN
+security create-keychain -p "$APPLE_PASSWORD" $KEYCHAIN_PATH
+security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
+security unlock-keychain -p "$APPLE_PASSWORD" $KEYCHAIN_PATH
 
 # import the certificate into the keychain
-security import $CERTIFICATE_CER -k $KEY_CHAIN -T /usr/bin/codesign;
-
-# set keychain settings
-security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
+security import $CERTIFICATE_PATH -P "$APPLE_PASSWORD" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
+security set-key-partition-list -S apple-tool:,apple: -k "$APPLE_PASSWORD" $KEYCHAIN_PATH
+security list-keychain -d user -s $KEYCHAIN_PATH
 
 # list signing certificates again
 security find-identity -v -p codesigning
 
 # remove certs
-rm -fr CERTIFICATE_CER
+rm -fr CERTIFICATE_PATH
+rm -fr PROFILE_PATH
