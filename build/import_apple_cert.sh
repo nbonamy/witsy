ebdf91b933bb5c80f092c91ad5e8b0144153dd68
Nicolas Bonamy <nicolas@bonamy.fr>
2024-10-20 20:49:38 -0500

    build scripts

--- build/import_apple_cert.sh
+++ build/import_apple_cert.sh
@@ -4,7 +4,7 @@ KEYCHAIN_PATH=$RUNNER_TEMP/build.keychain-db
 CERTIFICATE_PATH=$RUNNER_TEMP/certificate.p12
 
 # list signing certificates
-security find-identity -v -p codesigning
+#security find-identity -v -p codesigning
 
 # recreate the certificate from the secure environment variable
 echo $BUILD_CERTIFICATE_BASE64 | base64 --decode -o $CERTIFICATE_PATH
@@ -17,7 +17,7 @@ security unlock-keychain -p "$APPLE_PASSWORD" $KEYCHAIN_PATH
 # import the certificate into the keychain
 security import $CERTIFICATE_PATH -P "$APPLE_PASSWORD" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
 security set-key-partition-list -S apple-tool:,apple: -k "$APPLE_PASSWORD" $KEYCHAIN_PATH
-security list-keychain -d user -s $KEYCHAIN_PATH
+#security list-keychain -d user -s $KEYCHAIN_PATH
 
 # list signing certificates again
 security find-identity -v -p codesigning
