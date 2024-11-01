83d6dbcd48b30a1ec4795f31388cd854995fb177
Nicolas Bonamy <nicolas@bonamy.fr>
2024-11-01 14:32:36 -0500

    show generated image prompt

--- src/components/MessageItemImage.vue
+++ src/components/MessageItemImage.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="image-container">
     <img :src="url" :alt="desc" class="image" @click="onFullscreen" @load="onImageLoaded"/>
-    <BIconDownload class="download" @click="onDownload()" />
+    <div class="image-actions">
+      <BIconInfoCircle class="action info" v-if="prompt" @click="onInfo"/>
+      <BIconDownload class="action download" @click="onDownload" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 
+import Dialog from '../composables/dialog'
 import useEventBus from '../composables/event_bus'
 const { emitEvent } = useEventBus()
 
 const props = defineProps({
   url: String,
-  desc: String
+  desc: String,
+  prompt: String
 })
 
 const emits = defineEmits(['image-loaded'])
@@ -23,6 +28,13 @@ const onImageLoaded = () => {
 
 const onFullscreen = () => {
   emitEvent('fullscreen', props.url)
+}
+
+const onInfo = () => {
+  Dialog.show({
+    title: props.desc,
+    text: props.prompt,
+  })
 }
 
 const onDownload = () => {
