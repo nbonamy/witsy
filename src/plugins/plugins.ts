de42fab6aeacdec1cfda2f8d5660b8442e0597ed
Nicolas Bonamy <nicolas@bonamy.fr>
2024-11-04 22:13:43 -0600

    youtube plugin

--- src/plugins/plugins.ts
+++ src/plugins/plugins.ts
@@ -4,11 +4,13 @@ import BrowsePlugin from '../plugins/browse'
 import TavilyPlugin from '../plugins/tavily'
 import PythonPlugin from '../plugins/python'
 import ImagePlugin from '../plugins/image'
+import YouTubePlugin from '../plugins/youtube'
 import NestorPlugin from '../plugins/nestor'
 
 export const availablePlugins: anyDict = {
   image: ImagePlugin,
   browse: BrowsePlugin, 
+  youtube: YouTubePlugin,
   tavily: TavilyPlugin,
   python:  PythonPlugin,
   nestor: NestorPlugin,
