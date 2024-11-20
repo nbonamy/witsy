cc2fe5d5c633973f437e8f8f57449047b3bfe213
Nicolas Bonamy <nicolas@bonamy.fr>
2024-11-19 21:41:33 -0600

    image resize for attachments

---
+++ src/composables/image_utils.ts
@@ -0,0 +1,26 @@
+
+export default {
+
+  resize(src: string, maxSize: number, callback: (contents: string, mimeType: string) => void): void {
+
+    const img = document.createElement('img')
+    img.addEventListener('load', () => {
+      const canvas = document.createElement('canvas')
+      const ctx = canvas.getContext('2d')
+      if (img.width > img.height) {
+        canvas.width = maxSize
+        canvas.height = maxSize * img.height / img.width
+      } else {
+        canvas.height = maxSize
+        canvas.width = maxSize * img.width / img.height
+      }
+      console.log(`Image resized to ${canvas.width}x${canvas.height}`)
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      const dataURI = canvas.toDataURL('image/jpeg', 0.95);
+      callback(dataURI.split(',')[1], 'image/jpeg')
+    })
+    img.src = src
+  
+  }
+
+}
