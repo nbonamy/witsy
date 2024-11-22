c00fd1dde84fce9b8f5edd5b9ad09650e56c10f2
Nicolas Bonamy <nicolas@bonamy.fr>
2024-11-21 21:36:16 -0600

    fix

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -1,4 +1,12 @@
 
 export const wait = async (millis = 200) => {
-  await new Promise((resolve) => setTimeout(resolve, millis));
+  if (process.platform === 'win32') {
+    // for an unknown reason, the promise code started to fail on Windows
+    const waitTill = new Date(new Date().getTime() + millis);
+    while (waitTill > new Date()) {
+      // do nothing
+    }
+  } else {
+    await new Promise((resolve) => setTimeout(resolve, millis));
+  }
 }
