7a38b96088d8a1b9c8b7a37ccd314ef5f7302c75
Nicolas Bonamy <nicolas@bonamy.fr>
2024-11-21 21:46:16 -0600

    fix

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -1,12 +1,13 @@
 
 export const wait = async (millis = 200) => {
-  // if (process.platform === 'win32') {
-  //   // for an unknown reason, the promise code started to fail on Windows
-  //   const waitTill = new Date(new Date().getTime() + millis);
-  //   while (waitTill > new Date()) {
-  //     // do nothing
-  //   }
-  // } else {
+  if (process.env.DEBUG && process.platform === 'win32') {
+    // for an unknown reason, the promise code started to fail when debugging on Windows
+    const waitTill = new Date(new Date().getTime() + millis);
+    while (waitTill > new Date()) {
+      // do nothing
+    }
+  } else {
     await new Promise((resolve) => setTimeout(resolve, millis));
-  // }
+  }
 }
+
