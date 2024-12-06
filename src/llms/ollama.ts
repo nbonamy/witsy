213c623b0ce0c864cd684112dba61dd2e4ff759f
Nicolas Bonamy <nicolas@bonamy.fr>
2024-12-06 12:59:21 -0600

    llama 3.3 support

--- src/llms/ollama.ts
+++ src/llms/ollama.ts
@@ -1,8 +1,8 @@
 
 const getChatModels = [
+  { id: 'llama3.3:latest', name: 'Llama 3.3' },
   { id: 'llama3.2:latest', name: 'Llama 3.2' },
   { id: 'llama3.2-vision', name: 'Llama 3.2 Vision' },
-  { id: 'llama3.1:latest', name: 'Llama 3.1' },
   { id: 'mistral-large:latest', name: 'Mistral Large 2' },
   { id: 'phi3.5:latest', name: 'Phi 3.5' },
   { id: 'qwen2.5:14b', name: 'Qwen 2.5' },
