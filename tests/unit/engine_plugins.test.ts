bdf0e64c5e080e9b670ee6940b85ea32ce2b5532
Nicolas Bonamy <nicolas@bonamy.fr>
2024-12-16 15:39:28 -0600

    plugins tests

--- tests/unit/engine_plugins.test.ts
+++ tests/unit/engine_plugins.test.ts
@@ -5,11 +5,25 @@ import { store } from '../../src/services/store'
 import Browse from '../../src/plugins/browse'
 import Tavily from '../../src/plugins/tavily'
 import Python from '../../src/plugins/python'
+import YouTube from '../../src/plugins/youtube'
+import Nestor from '../../src/plugins/nestor'
 
 vi.mock('../../src/vendor/tavily', async () => {
   const Tavily = vi.fn()
   Tavily.prototype.search = vi.fn(() => ({ results: [ 'page1' ] }))
   return { default: Tavily }
+})
+
+vi.mock('youtube-transcript', async () => {
+  return { YoutubeTranscript: {
+    fetchTranscript: vi.fn(() => [ { text: 'line1' } ])
+  } }
+})
+
+vi.mock('ytv', async () => {
+  return { default: {
+    get_info: vi.fn(() => ({ title: 'title', channel_name: 'channel' }))
+  } }
 })
 
 beforeAll(() => {
@@ -26,6 +40,12 @@ beforeAll(() => {
   store.config.plugins.python = {
     enabled: true,
     binpath: 'python3',
+  }
+  store.config.plugins.youtube = {
+    enabled: true,
+  }
+  store.config.plugins.nestor = {
+    enabled: true,
   }
 })
 
@@ -70,4 +90,40 @@ test('Python Plugin', async () => {
   expect(python.getParameters()[0].required).toBe(true)
   expect(await python.execute({ script: 'print("hello")' })).toHaveProperty('result')
   expect((await python.execute({ script: 'print("hello")' })).result).toBe('bonjour')
+})
+
+test('YouTube Plugin', async () => {
+  const youtube = new YouTube(store.config.plugins.youtube)
+  expect(youtube.isEnabled()).toBe(true)
+  expect(youtube.getName()).toBe('get_youtube_transcript')
+  expect(youtube.getDescription()).not.toBeFalsy()
+  expect(youtube.getPreparationDescription()).toBeFalsy()
+  expect(youtube.getRunningDescription()).not.toBeFalsy()
+  expect(youtube.getParameters()[0].name).toBe('url')
+  expect(youtube.getParameters()[0].type).toBe('string')
+  expect(youtube.getParameters()[0].description).not.toBeFalsy()
+  expect(youtube.getParameters()[0].required).toBe(true)
+  expect(await youtube.execute({ url: 'test' })).toStrictEqual({
+    title: 'title',
+    channel: 'channel',
+    content: 'line1'
+  })
+})
+
+test('Nestor Plugin', async () => {
+  const nestor = new Nestor(store.config.plugins.nestor)
+  expect(nestor.isEnabled()).toBe(true)
+  expect(nestor.getName()).toBe('Nestor')
+  expect(nestor.isMultiTool()).toBe(true)
+  expect(await nestor.getTools()).toStrictEqual([
+    { function: { name: 'tool1' }, description: 'description1' },
+    { function: { name: 'tool2' }, description: 'description2' },
+  ])
+  expect(nestor.handlesTool('tool1')).toBe(true)
+  expect(nestor.handlesTool('tool2')).toBe(true)
+  expect(nestor.handlesTool('tool3')).toBe(false)
+  expect(await nestor.execute({ tool: 'tool1', parameters: { arg: 'hello' } })).toStrictEqual({
+    name: 'tool1',
+    params: { arg: 'hello' }
+  })
 })
