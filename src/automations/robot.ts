5f2d32a8aa0756b7ee65e36eefcc772453c984c6
Nicolas Bonamy <nicolas@bonamy.fr>
2024-12-17 21:16:44 -0600

    dynamic robot

--- src/automations/robot.ts
+++ src/automations/robot.ts
@@ -1,9 +1,25 @@
 
 import { Automator } from '../types/automation.d';
-//import { keyTap } from 'robotjs';
-//import { wait } from './utils';
+import { wait } from '../main/utils';
 
 export default class RobotAutomator implements Automator {
+
+  robot: any
+  
+  constructor() {
+    this.setup();
+  }
+
+  async setup() {
+    try {
+      const robotPackage = 'robotjs';
+      this.robot = await import(robotPackage);
+      return true
+    } catch {
+      console.log('Error loading robotjs. Disabling computer interaction.');
+      return false
+    }
+  }
   
   async getForemostAppId(): Promise<string> {
     throw new Error('Method not implemented.');
@@ -14,25 +30,29 @@ export default class RobotAutomator implements Automator {
   }
 
   async selectAll() {
-    // keyTap("a", process.platform === "darwin" ? "command" : "control");
-    // await wait();
+    if (!this.robot) throw new Error('Robotjs not loaded');
+    this.robot.keyTap("a", process.platform === "darwin" ? "command" : "control");
+    await wait();
   }
 
   async moveCaretBelow() {
-    // keyTap("right");
-    // keyTap("enter");
-    // keyTap("enter");
-    // await wait();
+    if (!this.robot) throw new Error('Robotjs not loaded');
+    this.robot.keyTap("right");
+    this.robot.keyTap("enter");
+    this.robot.keyTap("enter");
+    await wait();
   }
 
   async copySelectedText() {
-    // keyTap("c", process.platform === "darwin" ? "command" : "control");
-    // await wait();
+    if (!this.robot) throw new Error('Robotjs not loaded');
+    this.robot.keyTap("c", process.platform === "darwin" ? "command" : "control");
+    await wait();
   }
 
   async pasteText() {
-    // keyTap("v", process.platform === "darwin" ? "command" : "control");
-    // await wait();
+    if (!this.robot) throw new Error('Robotjs not loaded');
+    this.robot.keyTap("v", process.platform === "darwin" ? "command" : "control");
+    await wait();
   }
 
 }
