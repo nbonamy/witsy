6f345137079a7bed7307d1cfaa3988d69ca13901
Nicolas Bonamy <nicolas@bonamy.fr>
2024-12-22 23:16:58 -0600

    more tests

---
+++ tests/unit/tray.test.ts
@@ -0,0 +1,53 @@
+
+import { vi, beforeAll, expect, test } from 'vitest'
+import { app, Menu } from 'electron'
+import AutoUpdater from '../../src/main/autoupdate'
+import Tray from '../../src/main/tray'
+
+vi.mock('electron', () => {
+  const Tray = vi.fn();
+  Tray.prototype.setContextMenu = vi.fn();
+  Tray.prototype.on = vi.fn();
+  const Menu = {
+    buildFromTemplate: vi.fn(() => {}),
+  }
+  //Tray.prototype.show = vi.fn();
+  return {
+    app: {
+      getVersion: vi.fn(() => '1.0.0'),
+      getPath: vi.fn(() => ''),
+    },
+    autoUpdater: {
+      setFeedURL: vi.fn(),
+      checkForUpdates: vi.fn(),
+      on: vi.fn(),
+    },
+    nativeImage: {
+      createFromPath: vi.fn(() => {
+        return {
+          setTemplateImage: vi.fn(),
+        }
+      }),
+    },
+    Tray,
+    Menu,
+  }
+})
+
+beforeAll(() => {
+  // @ts-expect-error mocking
+  process.resourcesPath = 'resourcesPath'
+})
+
+test('Creates tray', async () => {
+  const tray = new Tray(app, new AutoUpdater({ onUpdateAvailable: () => {}, preInstall: () => {} }), () => {})
+  tray.install()
+  expect(tray.tray).toBeDefined()
+  expect(Menu.buildFromTemplate).toHaveBeenCalled()
+  expect(Menu.buildFromTemplate.mock.calls[0][0]).toHaveLength(11)
+  expect(Menu.buildFromTemplate.mock.calls[0][0].map((item: any) => item.label)).toEqual([
+    'Quick Prompt', 'New Chat', 'Realtime Chat', 'Scratchpad', 'Run AI Command', 'Read Aloud', 'Start Dictation', undefined, 'Settings…', undefined, 'Quit'
+  ]);
+  expect(tray.tray.setContextMenu).toHaveBeenCalled()
+  expect(tray.tray.on).toHaveBeenCalledTimes(2)
+});
