1eed95e62224a80c2b4111c2b05dd297f8183ce3
Nicolas Bonamy <nicolas@bonamy.fr>
2025-03-18 21:26:29 -0500

    https://github.com/nbonamy/witsy/discussions/99

---
+++ src/voice/stt-fal.ts
@@ -0,0 +1,70 @@
+
+import { Configuration } from 'types/config'
+import { STTEngine, ProgressCallback, TranscribeResponse } from './stt'
+import { fal } from '@fal-ai/client'
+
+export default class STTFal implements STTEngine {
+
+  config: Configuration
+
+  static readonly models = [
+    { id: 'fal-ai/whisper', label: 'Whisper' },
+    { id: 'fal-ai/wizper', label: 'Whisper v3 Large' },
+    { id: 'fal-ai/elevenlabs/speech-to-text', label: 'ElevenLabs' },
+  ]
+
+  constructor(config: Configuration) {
+    this.config = config
+  }
+
+  isReady(): boolean {
+    return true
+  }
+
+  static requiresDownload(): boolean {
+    return false
+  }
+
+  requiresDownload(): boolean {
+    return STTFal.requiresDownload()
+  }
+
+  async initialize(callback?: ProgressCallback): Promise<void> {
+    callback?.({ status: 'ready', task: 'fal.ai', model: this.config.stt.model })
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async transcribe(audioBlob: Blob, opts?: object): Promise<TranscribeResponse> {
+
+    // set api key
+    fal.config({
+      credentials: this.config.engines.falai.apiKey
+    });
+    
+    // call
+    const response = await fal.subscribe(this.config.stt.model, {
+      input: {
+        audio_url: new File([audioBlob], 'audio.webm', { type: audioBlob.type }),
+        ...(this.config.stt.locale ? { language: this.config.stt.locale?.substring(0, 2) } : {})
+      },
+    });
+
+    // return
+    return { text: response.data.text }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async isModelDownloaded(model: string): Promise<boolean> {
+    return false
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  deleteModel(model: string): Promise<void> {
+    return
+  }
+
+  deleteAllModels(): Promise<void> {
+    return
+  }
+
+}
--- src/voice/stt.ts
+++ src/voice/stt.ts
@@ -1,6 +1,7 @@
 
 import { Configuration } from 'types/config'
 import STTOpenAI from './stt-openai'
+import STTFal from './stt-fal'
 import STTGroq from './stt-groq'
 import STTWhisper from './stt-whisper'
 
@@ -51,6 +52,8 @@ const getSTTEngine = (config: Configuration): STTEngine => {
     return new STTOpenAI(config)
   } else if (engine === 'groq') {
     return new STTGroq(config)
+  } else if (engine === 'fal.ai') {
+    return new STTFal(config)
   } else if (engine === 'whisper') {
     return new STTWhisper(config)
   } else {
@@ -63,6 +66,8 @@ export const requiresDownload = (engine: string): boolean => {
     return STTOpenAI.requiresDownload()
   } else if (engine === 'groq') {
     return STTGroq.requiresDownload()
+  } else if (engine === 'fal.ai') {
+    return STTFal.requiresDownload()
   } else if (engine === 'whisper') {
     return STTWhisper.requiresDownload()
   } else {
