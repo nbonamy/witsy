47adc84e8b2f29d5597d84e9cb02033550ad504c
Nicolas Bonamy <nicolas@bonamy.fr>
2025-03-27 05:33:35 -0500

    https://github.com/nbonamy/witsy/issues/145 fal.ai custom parameters

--- src/studio/Settings.vue
+++ src/studio/Settings.vue
@@ -66,7 +66,8 @@
 
       <template v-if="showParams">
 
-        <div v-if="engine == 'replicate'" class="info"><a :href="`https://replicate.com/${model.split(':')[0]}`" target="_blank">{{ t('designStudio.moreAboutReplicateModels') }}</a></div>
+        <div v-if="engine == 'replicate'" class="info"><a :href="`https://replicate.com/${model.split(':')[0]}`" target="_blank">{{ t('designStudio.moreAboutModelParameters') }}</a></div>
+        <div v-if="engine == 'falai'" class="info"><a :href="`https://fal.ai/models//${model.split(':')[0]}`" target="_blank">{{ t('designStudio.moreAboutModelParameters') }}</a></div>
         <div v-if="engine == 'huggingface'" class="info">{{ t('designStudio.parameters.supportWarning') }}</div>
         <div v-if="engine == 'sdwebui'" class="info"><a :href="`${new SDWebUI(store.config).baseUrl}/docs#/default/text2imgapi_sdapi_v1_txt2img_post`" target="_blank">{{ t('designStudio.moreAboutSDWebUIParameters') }}</a></div>
         
@@ -232,46 +233,46 @@ const customParams = computed((): Parameter[] => {
     ]
   }
 
-  // fal.ai images
-  if (engine.value === 'falai' && mediaType.value === 'image') {
-    return [
-      { label: t('designStudio.parameters.size'),  key: 'image_size',  type: 'select', values: [
-        'square_hd', 'square', 'portrait_4_3', 'portrait_16_9', 'landscape_4_3', 'landscape_16_9'
-      ] },
-      { label: t('designStudio.parameters.style'),  key: 'style',  type: 'select', values: [
-        'realistic_image', 'digital_illustration', 'vector_illustration', 'realistic_image/b_and_w',
-        'realistic_image/hard_flash', 'realistic_image/hdr', 'realistic_image/natural_light',
-        'realistic_image/studio_portrait', 'realistic_image/enterprise', 'realistic_image/motion_blur',
-        'realistic_image/evening_light', 'realistic_image/faded_nostalgia', 'realistic_image/forest_life',
-        'realistic_image/mystic_naturalism', 'realistic_image/natural_tones', 'realistic_image/organic_calm',
-        'realistic_image/real_life_glow', 'realistic_image/retro_realism', 'realistic_image/retro_snapshot',
-        'realistic_image/urban_drama', 'realistic_image/village_realism', 'realistic_image/warm_folk',
-        'digital_illustration/pixel_art', 'digital_illustration/hand_drawn', 'digital_illustration/grain',
-        'digital_illustration/infantile_sketch', 'digital_illustration/2d_art_poster', 'digital_illustration/handmade_3d',
-        'digital_illustration/hand_drawn_outline', 'digital_illustration/engraving_color',
-        'digital_illustration/2d_art_poster_2', 'digital_illustration/antiquarian', 'digital_illustration/bold_fantasy',
-        'digital_illustration/child_book', 'digital_illustration/child_books', 'digital_illustration/cover',
-        'digital_illustration/crosshatch', 'digital_illustration/digital_engraving', 'digital_illustration/expressionism',
-        'digital_illustration/freehand_details', 'digital_illustration/grain_20', 'digital_illustration/graphic_intensity',
-        'digital_illustration/hard_comics', 'digital_illustration/long_shadow', 'digital_illustration/modern_folk',
-        'digital_illustration/multicolor', 'digital_illustration/neon_calm', 'digital_illustration/noir',
-        'digital_illustration/nostalgic_pastel', 'digital_illustration/outline_details', 'digital_illustration/pastel_gradient',
-        'digital_illustration/pastel_sketch', 'digital_illustration/pop_art', 'digital_illustration/pop_renaissance',
-        'digital_illustration/street_art', 'digital_illustration/tablet_sketch', 'digital_illustration/urban_glow',
-        'digital_illustration/urban_sketching', 'digital_illustration/vanilla_dreams', 'digital_illustration/young_adult_book',
-        'digital_illustration/young_adult_book_2', 'vector_illustration/bold_stroke', 'vector_illustration/chemistry',
-        'vector_illustration/colored_stencil', 'vector_illustration/contour_pop_art', 'vector_illustration/cosmics',
-        'vector_illustration/cutout', 'vector_illustration/depressive', 'vector_illustration/editorial',
-        'vector_illustration/emotional_flat', 'vector_illustration/infographical', 'vector_illustration/marker_outline',
-        'vector_illustration/mosaic', 'vector_illustration/naivector', 'vector_illustration/roundish_flat',
-        'vector_illustration/segmented_colors', 'vector_illustration/sharp_contrast', 'vector_illustration/thin',
-        'vector_illustration/vector_photo', 'vector_illustration/vivid_shapes', 'vector_illustration/engraving',
-        'vector_illustration/line_art', 'vector_illustration/line_circuit', 'vector_illustration/linocut'
-      ] },
-      // { label: t('designStudio.parameters.width'),  key: 'image_size.width',  type: 'input' },
-      // { label: t('designStudio.parameters.height'), key: 'image_size.height', type: 'input' },
-    ]
-  }
+  // // fal.ai images
+  // if (engine.value === 'falai' && mediaType.value === 'image') {
+  //   return [
+  //     { label: t('designStudio.parameters.size'),  key: 'image_size',  type: 'select', values: [
+  //       'square_hd', 'square', 'portrait_4_3', 'portrait_16_9', 'landscape_4_3', 'landscape_16_9'
+  //     ] },
+  //     { label: t('designStudio.parameters.style'),  key: 'style',  type: 'select', values: [
+  //       'realistic_image', 'digital_illustration', 'vector_illustration', 'realistic_image/b_and_w',
+  //       'realistic_image/hard_flash', 'realistic_image/hdr', 'realistic_image/natural_light',
+  //       'realistic_image/studio_portrait', 'realistic_image/enterprise', 'realistic_image/motion_blur',
+  //       'realistic_image/evening_light', 'realistic_image/faded_nostalgia', 'realistic_image/forest_life',
+  //       'realistic_image/mystic_naturalism', 'realistic_image/natural_tones', 'realistic_image/organic_calm',
+  //       'realistic_image/real_life_glow', 'realistic_image/retro_realism', 'realistic_image/retro_snapshot',
+  //       'realistic_image/urban_drama', 'realistic_image/village_realism', 'realistic_image/warm_folk',
+  //       'digital_illustration/pixel_art', 'digital_illustration/hand_drawn', 'digital_illustration/grain',
+  //       'digital_illustration/infantile_sketch', 'digital_illustration/2d_art_poster', 'digital_illustration/handmade_3d',
+  //       'digital_illustration/hand_drawn_outline', 'digital_illustration/engraving_color',
+  //       'digital_illustration/2d_art_poster_2', 'digital_illustration/antiquarian', 'digital_illustration/bold_fantasy',
+  //       'digital_illustration/child_book', 'digital_illustration/child_books', 'digital_illustration/cover',
+  //       'digital_illustration/crosshatch', 'digital_illustration/digital_engraving', 'digital_illustration/expressionism',
+  //       'digital_illustration/freehand_details', 'digital_illustration/grain_20', 'digital_illustration/graphic_intensity',
+  //       'digital_illustration/hard_comics', 'digital_illustration/long_shadow', 'digital_illustration/modern_folk',
+  //       'digital_illustration/multicolor', 'digital_illustration/neon_calm', 'digital_illustration/noir',
+  //       'digital_illustration/nostalgic_pastel', 'digital_illustration/outline_details', 'digital_illustration/pastel_gradient',
+  //       'digital_illustration/pastel_sketch', 'digital_illustration/pop_art', 'digital_illustration/pop_renaissance',
+  //       'digital_illustration/street_art', 'digital_illustration/tablet_sketch', 'digital_illustration/urban_glow',
+  //       'digital_illustration/urban_sketching', 'digital_illustration/vanilla_dreams', 'digital_illustration/young_adult_book',
+  //       'digital_illustration/young_adult_book_2', 'vector_illustration/bold_stroke', 'vector_illustration/chemistry',
+  //       'vector_illustration/colored_stencil', 'vector_illustration/contour_pop_art', 'vector_illustration/cosmics',
+  //       'vector_illustration/cutout', 'vector_illustration/depressive', 'vector_illustration/editorial',
+  //       'vector_illustration/emotional_flat', 'vector_illustration/infographical', 'vector_illustration/marker_outline',
+  //       'vector_illustration/mosaic', 'vector_illustration/naivector', 'vector_illustration/roundish_flat',
+  //       'vector_illustration/segmented_colors', 'vector_illustration/sharp_contrast', 'vector_illustration/thin',
+  //       'vector_illustration/vector_photo', 'vector_illustration/vivid_shapes', 'vector_illustration/engraving',
+  //       'vector_illustration/line_art', 'vector_illustration/line_circuit', 'vector_illustration/linocut'
+  //     ] },
+  //     // { label: t('designStudio.parameters.width'),  key: 'image_size.width',  type: 'input' },
+  //     // { label: t('designStudio.parameters.height'), key: 'image_size.height', type: 'input' },
+  //   ]
+  // }
 
   // hugingface all models
   if (engine.value === 'huggingface') {
@@ -301,7 +302,7 @@ const modelHasCustomParams = computed(() => {
 })
 
 const modelHasUserParams = computed(() => {
-  return ['replicate', 'sdwebui']. includes(engine.value)
+  return ['replicate', 'falai', 'sdwebui']. includes(engine.value)
 })
 
 const modelHasParams = computed(() => {
