9d050cd858b31d38cf3ae663d73e68bb7e38668e
Nicolas Bonamy <nicolas@bonamy.fr>
2025-03-29 16:50:53 -0500

    test fix

--- tests/unit/window.test.ts
+++ tests/unit/window.test.ts
@@ -35,6 +35,7 @@ vi.mock('electron', async () => {
   BrowserWindow.prototype.setBounds = vi.fn()
   BrowserWindow.prototype.setSize = vi.fn()
   BrowserWindow.prototype.setOpacity = vi.fn()
+  BrowserWindow.prototype.setIgnoreMouseEvents = vi.fn()
   BrowserWindow['getAllWindows'] = vi.fn(() => {
     const window1 = new BrowserWindow()
     const window2 = new BrowserWindow()
