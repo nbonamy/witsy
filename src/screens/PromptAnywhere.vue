1e017a653bc129a9a50fa9c39d8456ff6492a1c7
Nicolas Bonamy <nicolas@bonamy.fr>
2025-04-02 23:16:23 -0500

    https://github.com/nbonamy/witsy/issues/161

--- src/screens/PromptAnywhere.vue
+++ src/screens/PromptAnywhere.vue
@@ -434,7 +434,10 @@ const saveChat = async () => {
 
   // we need a title
   if (!chat.value.title) {
-    const title = await llm.complete(chat.value.model, [...chat.value.messages, new Message('user', i18nInstructions(store.config, 'instructions.titlingUser'))])
+    const title = await llm.complete(chat.value.model, [
+      ...chat.value.messages,
+      new Message('user', i18nInstructions(store.config, 'instructions.titlingUser'))
+    ], { tools: false })
     chat.value.title = title.content
   }
 
