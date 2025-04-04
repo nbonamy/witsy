18fdb7404de6fd2986911a87189fbb9b51f0c159
dvanp <dvanp@users.noreply.github.com>
2025-04-04 16:18:36 +0200

    Correct tool name extraction in callTool in line with changed uniqueToolName

--- src/main/mcp.ts
+++ src/main/mcp.ts
@@ -404,7 +404,7 @@ export default class {
       throw new Error(`Tool ${name} not found`)
     }
 
-    const tool = name.replace(`___${client.server.uuid}`, '')
+    const tool = name.replace(`___${client.server.uuid.slice(-4)}`, '')
     console.log('Calling MCP tool', tool, args)
 
     return await client.client.callTool({
@@ -416,7 +416,7 @@ export default class {
 
   protected uniqueToolName(server: McpServer, name: string): string {
   const suffix = server.uuid.slice(-4)  // use only last 4 characters of UUID to generate uniqueToolName
-  return `${name}_${suffix}`
+  return `${name}___${suffix}`
 }
 
   protected mcpToOpenAI = (server: McpServer, tool: any): LlmTool => {
