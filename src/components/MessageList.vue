09d01769507e38fb1c41af2ab1ebd29e2450411c
Nicolas Bonamy <nicolas@bonamy.fr>
2025-05-14 21:59:54 -0500

    scrolling fix

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -87,8 +87,9 @@ const onNewChunk = async (chunk: LlmChunk) => {
   if (avatar && divScroller.value) {
     const rc = avatar.getBoundingClientRect()
     const messagesListRect = divScroller.value.getBoundingClientRect()
-    if (rc && messagesListRect && rc.top - rc.height < messagesListRect.top) {
+    if (rc && messagesListRect && rc.top - rc.height * 1.5 < messagesListRect.top) {
       scrollOnChunk = false
+      overflown.value = true
     }
   }
 
