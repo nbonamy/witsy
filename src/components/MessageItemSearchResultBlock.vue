fcbfbe4f78ed2cd86ef0e50c5346c7c30075f8e1
Nicolas Bonamy <nicolas@bonamy.fr>
2025-06-23 21:49:12 -0500

    tool search result expanded

--- src/components/MessageItemSearchResultBlock.vue
+++ src/components/MessageItemSearchResultBlock.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="tool-search-result">
+  <div v-if="!expanded" class="tool-search-result" @click="expanded = true">
     <div class="tool-search-result-icons">
       <img v-for="result in toolCall.result.results" :src="getFaviconUrl(result.url)" class="favicon" @error="onFaviconError" />
     </div>
     {{ t('plugins.search.completed', { query: toolCall.params.query, count: toolCall.result.results.length }) }}
+  </div>
+  <div v-else class="expanded">
+    <div class="header" @click="expanded = false">
+      <BIconXLg class="close" />
+      {{ t('plugins.search.completed', { query: toolCall.params.query, count: toolCall.result.results.length }) }}
+    </div>
+    <MessageItemSearchToolBlock class="results" :tool-call="toolCall"  />
   </div>
 </template>
 
 <script setup lang="ts">
 
 import { ToolCall } from '../types/index'
+import { ref } from 'vue'
 import { t } from '../services/i18n'
+import MessageItemSearchToolBlock from './MessageItemSearchToolBlock.vue' 
 
 defineProps({
   toolCall: {
@@ -18,6 +27,8 @@ defineProps({
     required: true,
   },
 })
+
+const expanded = ref(false)
 
 const getFaviconUrl = (url: string): string => {
   return `https://s2.googleusercontent.com/s2/favicons?domain_url=${url}`
@@ -33,13 +44,14 @@ const onFaviconError = (event: Event) => {
 <style scoped>
 
 .tool-search-result {
+  margin: 1rem 0px;
   padding: 0.5rem 0px;
-  padding-top: 1rem;
   display: flex;
   align-items: center;
   gap: 0.5rem;
   color: var(--faded-text-color);
   font-size: .9em;
+  cursor: pointer;
 }
 
 .tool-search-result-icons {
@@ -58,6 +70,31 @@ const onFaviconError = (event: Event) => {
 
   &:first-child {
     margin-left: 0px;
+  }
+}
+
+.expanded {
+  margin: 1rem 0px;
+  border: 1px solid var(--control-border-color);
+  border-radius: 8px;
+  position: relative;
+
+  .header {
+    background-color: color-mix(in srgb, var(--control-border-color), transparent 80%);
+    padding: 0.5rem 1rem;
+    font-size: .9em;
+    cursor: pointer;
+  }
+
+  .close {
+    position: absolute;
+    top: 0.5rem;
+    right: 0.5rem;
+    color: var(--dimmed-text-color);
+  }
+
+  .results {
+    padding: 1rem;
   }
 }
 
