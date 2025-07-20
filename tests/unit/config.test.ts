75ae71f5a909753350dd0b43db357e1d2a7efd23
Nicolas Bonamy <nicolas@bonamy.fr>
2025-07-20 09:38:59 -0500

    config engine load/save tests

--- tests/unit/config.test.ts
+++ tests/unit/config.test.ts
@@ -1,7 +1,7 @@
 
 import { app } from 'electron'
 import { Configuration } from '../../src/types/config'
-import { vi, expect, test } from 'vitest'
+import { vi, expect, test, Mock } from 'vitest'
 import * as config from '../../src/main/config'
 import defaultSettings from '../../defaults/settings.json'
 import fs from 'fs'
@@ -9,7 +9,7 @@ import fs from 'fs'
 vi.mock('electron', async () => {
   return {
     app: {
-      getPath: vi.fn(() => '')
+      getPath: vi.fn(() => ''),
     }
   }
 })
@@ -32,6 +32,13 @@ test('Load default settings', () => {
   expect(loaded).toStrictEqual(defaultSettings)
   expect(loaded.general.locale).toBe('')
   expect(loaded.engines.openai.models.chat).toStrictEqual([])
+})
+
+test('Load engine config files', () => {
+  app.getPath = vi.fn(() => './tests/fixtures/engines')
+  const loaded = config.loadSettings(app)
+  expect(loaded.engines.openai.apiKey).toBe('openai-api-key')
+  expect(loaded.engines.openai.models.chat).toHaveLength(1)
 })
 
 test('Load settings with error', () => {
@@ -68,4 +75,19 @@ test('Save settings', () => {
   config.saveSettings('settings.json', loaded)
   const saved = JSON.parse(JSON.stringify(loaded))
   expect(fs.writeFileSync).toHaveBeenLastCalledWith('settings.json', JSON.stringify(saved, null, 2))
+})
+
+test('Save engine config', () => {
+  app.getPath = vi.fn(() => './tests/fixtures/engines')
+  const loaded = config.loadSettings(app)
+  config.saveSettings(app, loaded)
+  expect(fs.writeFileSync).toHaveBeenCalledWith('tests/fixtures/engines/engines/openai.json', expect.any(String))
+  expect(fs.writeFileSync).toHaveBeenCalledWith('tests/fixtures/engines/settings.json', expect.any(String))
+  // @ts-expect-error mock type stuff
+  const calls = (fs.writeFileSync.mock as Mock).calls
+  const openai = JSON.parse(calls[1][1])
+  expect(openai.models.chat).toHaveLength(1)
+  const settings = JSON.parse(calls[calls.length - 1][1])
+  expect(settings.engines.openai.apiKey).toBe('openai-api-key')
+  expect(settings.engines.openai.models).toBeUndefined()
 })
