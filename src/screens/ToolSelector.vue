5876df9b77bbcb25a98af78f890f30dd63ee019b
Nicolas Bonamy <nicolas@bonamy.fr>
2025-07-28 17:01:12 -0500

    tool table component

--- src/screens/ToolSelector.vue
+++ src/screens/ToolSelector.vue
@@ -4,24 +4,7 @@
       {{ t('toolSelector.title') }}
     </template>
     <template #body>
-      <div class="tools sticky-table-container">
-        <table>
-          <thead>
-            <tr>
-              <th>&nbsp;</th>
-              <th>{{ t('toolSelector.tools.name') }}</th>
-              <th>{{ t('toolSelector.tools.description') }}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="tool in tools" :key="tool.id" class="tool">
-              <td class="tool-enabled"><input type="checkbox" :checked="isToolActive(tool)" @click="toggleTool(tool)" /></td>
-              <td class="tool-name">{{ tool.name }}</td>
-              <td class="tool-description"><div>{{ tool.description }}</div></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <ToolTable v-model="selection" @toggle="selection = toolTable.toggleTool(selection, $event)" ref="toolTable" />
     </template>
     <template #footer>
       <div class="buttons">
@@ -38,23 +21,13 @@
 
 import { ToolSelection } from '../types/llm'
 import { ref, onMounted, watch, PropType } from 'vue'
-import { store } from '../services/store'
 import { t } from '../services/i18n'
-import { availablePlugins, PluginInstance } from '../plugins/plugins'
-import { Plugin } from 'multi-llm-ts'
 import ModalDialog from '../components/ModalDialog.vue'
-import McpPlugin from '../plugins/mcp'
-
-type Tool = {
-  id: string
-  name: string
-  description: string
-  plugin: Plugin
-}
+import ToolTable from '../components/ToolTable.vue'
 
 const dialog = ref(null)
-const tools= ref<Tool[]>([])
-const selection= ref<string[]>([])
+const toolTable = ref(null)
+const selection = ref<string[]>([])
 
 const props = defineProps({
   tools: {
@@ -71,60 +44,6 @@ onMounted(async () => {
   }, { immediate: true }) 
 })
 
-const initTools = async () => {
-  tools.value = []
-  for (const pluginName in availablePlugins) {
-    
-    const pluginClass = availablePlugins[pluginName]
-    const plugin: PluginInstance = new pluginClass(store.config.plugins[pluginName])
-    if ('getTools' in plugin) {
-
-      const pluginTools = await plugin.getTools()
-      for (const pluginTool of pluginTools) {
-
-        const id = pluginTool.function.name
-        const name = plugin instanceof McpPlugin
-          ? window.api.mcp.originalToolName(id)
-          : id
-
-        tools.value.push({
-          id, name,
-          description: pluginTool.function.description,
-          plugin
-        })
-      }
-
-    } else {
-      tools.value.push({
-        id: plugin.getName(),
-        name: plugin.getName(),
-        description: plugin.getDescription(),
-        plugin
-      })
-    }
-  }
-}
-
-const toggleTool = (tool: Tool) => {
-
-  // if all tools enabled then fill
-  if (!selection.value) {
-    selection.value = tools.value.map(t => t.id)
-  }
-
-  // toggle the tool
-  const index = selection.value.findIndex(t => t === tool.id)
-  if (index > -1) {
-    selection.value.splice(index, 1)
-  } else {
-    selection.value.push(tool.id)
-  }
-}
-
-const isToolActive = (tool: Tool) => {
-  return !selection.value || selection.value.includes(tool.id)
-}
-
 const close = () => {
   dialog.value.close()
 }
@@ -139,8 +58,8 @@ const onSave = () => {
 }
 
 defineExpose({
-  show: () => {
-    initTools()
+  show: async () => {
+    await toolTable.value.initTools()
     dialog.value.show()
   },
   close,
