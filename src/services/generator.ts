2119670cd6b7ef292d5e2d93cdb05bbf3be9e8f5
Nicolas Bonamy <nicolas@bonamy.fr>
2025-10-24 14:34:51 -0500

    https://github.com/nbonamy/witsy/issues/458

--- src/services/generator.ts
+++ src/services/generator.ts
@@ -300,9 +300,9 @@ export default class Generator {
           if (opts?.contextWindowSize || opts?.maxTokens || opts?.temperature || opts?.top_k || opts?.top_p || Object.keys(opts?.customOpts || {}).length > 0) {
             response.setText(t('generator.errors.tryWithoutParams'))
           } else if (llm.plugins.length > 0) {
-            response.setText(t('generator.errors.tryWithoutPlugins'))
+            response.setText(t('generator.errors.tryWithoutPlugins', { error: error.message }))
           } else {
-            response.setText(t('generator.errors.couldNotGenerate'))
+            response.setText(t('generator.errors.couldNotGenerate', { error: error.message }))
           }
         } else {
           response.appendText({ type: 'content', text: t('generator.errors.cannotContinue'), done: true })
