23c04f018d3d1b78e6fcade61544a73b27b8d130
Nicolas Bonamy <nicolas@bonamy.fr>
2025-10-24 15:12:04 -0500

    fix: expert stuff

--- src/types/index.ts
+++ src/types/index.ts
@@ -254,6 +254,11 @@ export type Expert = {
   }
 }
 
+export type ExpertData = {
+  categories: ExpertCategory[]
+  experts: Expert[]
+}
+
 export type ComputerAction = {
   action: 'key' | 'type' | 'mouse_move' | 'left_click' | 'left_click_drag' | 'right_click' | 'middle_click' | 'double_click' | 'screenshot' | 'cursor_position'
   coordinate?: number[]
