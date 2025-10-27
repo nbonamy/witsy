9572050cab1649cc7174ee63c6ca2dcc095d3e5c
Nicolas Bonamy <nicolas@bonamy.fr>
2025-10-27 18:47:03 -0500

    chore: dependency upgrade

--- src/components/OllamaModelPull.vue
+++ src/components/OllamaModelPull.vue
@@ -32,7 +32,7 @@ import Dialog from '../composables/dialog'
 import Combobox from './Combobox.vue'
 import ModelSelectPlus from './ModelSelectPlus.vue'
 import { ProgressResponse } from 'ollama'
-import type { A as AbortableAsyncIterator } from 'ollama/dist/shared/ollama.415fa141.cjs'
+import type { A as AbortableAsyncIterator } from 'ollama/dist/shared/ollama.27169772.cjs'
 
 type Model = {
   id: string,
