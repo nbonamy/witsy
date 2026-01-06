06647207a4e279834b06f0338ec0d815f09f711b
Nicolas Bonamy <nicolas@bonamy.fr>
2026-01-06 16:50:48 +0000

    fix: scheduler layout

--- src/renderer/components/Scheduler.vue
+++ src/renderer/components/Scheduler.vue
@@ -3,67 +3,67 @@
 
   <div class="scheduler">
 
-    <select name="interval" v-model="interval" @change="save">
-      <option value="">{{ t('common.schedule.disabled') }}</option>
-      <option value="m">{{ t('common.schedule.minutes') }}</option>
-      <option value="h">{{ t('common.schedule.hourly') }}</option>
-      <option value="d">{{ t('common.schedule.daily') }}</option>
-      <option value="w">{{ t('common.schedule.weekly') }}</option>
-      <option value="M">{{ t('common.schedule.monthly') }}</option>
+    <select name="interval" v-model="interval" @change="save" :disabled="disabled">
+      <option v-if="intervals.includes('')" value="">{{ t('common.schedule.disabled') }}</option>
+      <option v-if="intervals.includes('m')" value="m">{{ t('common.schedule.minutes') }}</option>
+      <option v-if="intervals.includes('h')" value="h">{{ t('common.schedule.hourly') }}</option>
+      <option v-if="intervals.includes('d')" value="d">{{ t('common.schedule.daily') }}</option>
+      <option v-if="intervals.includes('w')" value="w">{{ t('common.schedule.weekly') }}</option>
+      <option v-if="intervals.includes('M')" value="M">{{ t('common.schedule.monthly') }}</option>
     </select>
 
     <template v-if="interval === 'm'">
       <span>{{ t('common.schedule.every') }}</span>
-      <input type="number" name="every" v-model.number="every" min="1" max="59" step="1" @change="save" />
+      <input type="number" name="every" v-model.number="every" min="1" max="59" step="1" @input="save" />
       <span>{{ t('common.schedule.minutes') }}</span>
     </template>
 
     <template v-else-if="interval === 'h'">
       <span>{{ t('common.schedule.every') }}</span>
-      <input type="number" name="every" v-model.number="every" min="1" max="23" step="1" @change="save" />
+      <input type="number" name="every" v-model.number="every" min="1" max="23" step="1" @input="save" />
       <span>{{ t('common.schedule.hours_on_minute') }}</span>
-      <input type="number" name="on" v-model.number="on" min="0" max="59" step="1" @change="save" />
+      <input type="number" name="on" v-model.number="on" min="0" max="59" step="1" @input="save" />
     </template>
 
     <template v-else-if="interval === 'd'">
       <span>{{ t('common.schedule.every') }}</span>
-      <input type="number" name="every" v-model.number="every" min="1" max="31" step="1" @change="save" />
+      <input type="number" name="every" v-model.number="every" min="1" max="31" step="1" @input="save" />
       <span>{{ t('common.schedule.days_at') }}</span>
-      <input type="time" class="time" name="at" v-model="at" @change="save" />
+      <input type="time" class="time" name="at" v-model="at" @input="save" />
     </template>
 
-    <table v-else-if="interval === 'w'"><tbody>
-      <tr>
-        <td>{{ t('common.schedule.every') }}</td>
-        <td class="nowrap">
-          <input type="checkbox" id="schedule-mon" name="mon" :checked="weekDays.includes(1)" @change="onDay(1)" /><label for="schedule-mon" class="no-colon">{{ t('common.schedule.monday') }}</label>
-          <input type="checkbox" id="schedule-tue" name="tue" :checked="weekDays.includes(2)" @change="onDay(2)" /><label for="schedule-tue" class="no-colon">{{ t('common.schedule.tuesday') }}</label>
-          <input type="checkbox" id="schedule-wed" name="wed" :checked="weekDays.includes(3)" @change="onDay(3)" /><label for="schedule-wed" class="no-colon">{{ t('common.schedule.wednesday') }}</label>
-          <input type="checkbox" id="schedule-thu" name="thu" :checked="weekDays.includes(4)" @change="onDay(4)" /><label for="schedule-thu" class="no-colon">{{ t('common.schedule.thursday') }}</label>
-          <input type="checkbox" id="schedule-fri" name="fri" :checked="weekDays.includes(5)" @change="onDay(5)" /><label for="schedule-fri" class="no-colon">{{ t('common.schedule.friday') }}</label>
-          <input type="checkbox" id="schedule-sat" name="sat" :checked="weekDays.includes(6)" @change="onDay(6)" /><label for="schedule-sat" class="no-colon">{{ t('common.schedule.saturday') }}</label>
-          <input type="checkbox" id="schedule-sun" name="sun" :checked="weekDays.includes(0)" @change="onDay(0)" /><label for="schedule-sun" class="no-colon">{{ t('common.schedule.sunday') }}</label>
-        </td>
-      </tr>
-      <tr>
-        <td>{{ t('common.schedule.at') }}</td>
-        <td><input type="time" class="time" name="at" v-model="at" @change="save" /></td>
-      </tr>
-    </tbody></table>
+    <div v-else-if="interval === 'w'" class="weekly">
+      <div class="row">
+        <span class="label">{{ t('common.schedule.every') }}</span>
+        <div class="days-grid">
+          <div class="day"><input type="checkbox" id="schedule-mon" name="mon" :checked="weekDays.includes(1)" @change="onDay(1)" /><label for="schedule-mon" class="no-colon">{{ t('common.schedule.monday') }}</label></div>
+          <div class="day"><input type="checkbox" id="schedule-tue" name="tue" :checked="weekDays.includes(2)" @change="onDay(2)" /><label for="schedule-tue" class="no-colon">{{ t('common.schedule.tuesday') }}</label></div>
+          <div class="day"><input type="checkbox" id="schedule-wed" name="wed" :checked="weekDays.includes(3)" @change="onDay(3)" /><label for="schedule-wed" class="no-colon">{{ t('common.schedule.wednesday') }}</label></div>
+          <div class="day"><input type="checkbox" id="schedule-thu" name="thu" :checked="weekDays.includes(4)" @change="onDay(4)" /><label for="schedule-thu" class="no-colon">{{ t('common.schedule.thursday') }}</label></div>
+          <div class="day"><input type="checkbox" id="schedule-fri" name="fri" :checked="weekDays.includes(5)" @change="onDay(5)" /><label for="schedule-fri" class="no-colon">{{ t('common.schedule.friday') }}</label></div>
+          <div class="day"><input type="checkbox" id="schedule-sat" name="sat" :checked="weekDays.includes(6)" @change="onDay(6)" /><label for="schedule-sat" class="no-colon">{{ t('common.schedule.saturday') }}</label></div>
+          <div class="day"><input type="checkbox" id="schedule-sun" name="sun" :checked="weekDays.includes(0)" @change="onDay(0)" /><label for="schedule-sun" class="no-colon">{{ t('common.schedule.sunday') }}</label></div>
+        </div>
+      </div>
+      <div class="row">
+        <span class="label">{{ t('common.schedule.at') }}</span>
+        <input type="time" class="time" name="at" v-model="at" @input="save" />
+      </div>
+    </div>
 
     <table v-else-if="interval === 'M'"><tbody>
       <tr>
         <td><span>{{ t('common.schedule.on_the') }}</span></td>
         <td class="nowrap">
-          <input type="number" name="on" v-model.number="on" min="1" max="31" step="1" @change="save" />
-          <span>{{ t('common.schedule.day_of_every') }}</span>
-          <input type="number" name="every" v-model.number="every" min="1" max="12" step="1" @change="save" />
-          <span>{{ t('common.schedule.months') }}</span>
+          <input type="number" name="on" v-model.number="on" min="1" max="31" step="1" @input="save" />
+          <span style="margin: 0 4px;">{{ t('common.schedule.day_of_every') }}</span>
+          <input type="number" name="every" v-model.number="every" min="1" max="12" step="1" @input="save" />
+          <span style="margin-left: 4px;">{{ t('common.schedule.months') }}</span>
         </td>
       </tr>
       <tr>
         <td><span>{{ t('common.schedule.at') }}</span></td>
-        <td><input type="time" class="time" name="at" v-model="at" @change="save" /></td>
+        <td><input type="time" class="time" name="at" v-model="at" @input="save" /></td>
       </tr>
     </tbody></table>
 
@@ -79,6 +79,19 @@ import { t } from '@services/i18n'
 const schedule = defineModel({
   type: String,
   default: '',
+})
+
+export type SchedulerInterval = '' | 'm' | 'h' | 'd' | 'w' | 'M'
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  intervals: {
+    type: Array as () => SchedulerInterval[],
+    default: () => ['', 'm', 'h', 'd', 'w', 'M'],
+  },
 })
 
 const interval = ref('')
@@ -259,15 +272,50 @@ const save = () => {
   }
 
   input[type=time] {
-    max-width: 96px !important;
+    max-width: 128px !important;
     text-align: center;
+  }
+
+  .weekly {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    .row {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: 12px;
+
+      .label {
+        min-width: 40px;
+        text-align: right;
+      }
+    }
+
+    .days-grid {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      gap: 4px 8px;
+
+      .day {
+        display: flex;
+        align-items: center;
+        white-space: nowrap;
+
+        input[type=checkbox] {
+          margin: 0;
+        }
+
+        label {
+          margin-left: 4px;
+        }
+      }
+    }
   }
 
   &:has(table) {
     align-items: flex-start;
-    select {
-      margin-top: 6px !important;
-    }
   }
 
   table {
@@ -283,33 +331,13 @@ const save = () => {
         white-space: nowrap;
       }
 
+      span {
+        margin-top: 0px;
+      }
+
       input[type=number],
       input[type=time] {
         margin: 0px 4px;
-      }
-
-      input[type=checkbox] {
-        margin-top: 0px;
-        margin-bottom: 0px;
-        position: relative;
-        top: -4px;
-      }
-
-      input[type=checkbox]:checked {
-        margin-top: 0px;
-        margin-bottom: 0px;
-        position: relative;
-        top: -5px;
-      }
-
-      input[type=checkbox]:checked:after {
-        left: 0px !important;
-      }
-
-      label {
-        margin-right: 4px;
-        position: relative;
-        top: -6px;
       }
     }
   }
