1b6f5cffb5073df956c8b0979d263082a2615c59
Nicolas Bonamy <nicolas@bonamy.fr>
2026-01-16 11:11:15 -0600

    chore: reporter fix

--- tests/reporter/silent.ts
+++ tests/reporter/silent.ts
@@ -1,9 +1,13 @@
-
 import { DotReporter } from 'vitest/reporters'
 
 export default class SilentReporter extends DotReporter {
 
   onTestCaseReady() {}
   onTestCaseResult() {}
+
+  onTestRunEnd() {
+    // Delay to ensure this prints after DotReporter's summary
+    setTimeout(() => console.log('[TESTS:DONE]'), 100)
+  }
 
 }
